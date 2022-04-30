a313b8b6ef376caf051d8f40b3b8557a9f0befb5
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2022-05-01 01:21:38 +0300

    Add preview of pdf file

--- client/src/components/drop-file-input/DropFileInput.js
+++ client/src/components/drop-file-input/DropFileInput.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
  
 import React,{Component} from 'react';
- 
+// Import the main component
+import{Viewer, Worker}from'@react-pdf-viewer/core';     
+
+// Import the styles
+import'@react-pdf-viewer/core/lib/styles/index.css'; 
+
 class App extends Component {
   
     state = {
       // Initially, no file is selected
       selectedFile: null,
-      data: ""
+      data: "",
+      type: ""
     };
 
     fileToBase64 = async(file,cb) => {
@@ -26,14 +32,13 @@ class App extends Component {
       if(target.files < 1 || !target.validity.valid){
         return
       }
-      this.setState({ selectedFile: target.files[0] });
+      this.setState({ selectedFile: target.files[0], type: target.files[0].type });
       this.fileToBase64(target.files[0],(err,result) => {
         if (result){
           this.setState({ data: result });
-          console.log(result)
         }
       })
-      console.log("file chosen") 
+      console.log("file chosen")
     };
     
     // On file upload (click the upload button)
@@ -56,6 +61,57 @@ class App extends Component {
           })
         }
       };
+
+      // https://react-pdf-viewer.dev/examples/preview-a-pdf-file-from-base-64/
+
+      base64toBlob = (data) => {
+        // Cut the prefix `data:application/pdf;base64` from the raw base 64
+        const base64WithoutPrefix = data.substr('data:application/pdf;base64,'.length);
+    
+        const bytes = atob(base64WithoutPrefix);
+        let length = bytes.length;
+        let out = new Uint8Array(length);
+    
+        while (length--) {
+            out[length] = bytes.charCodeAt(length);
+        }
+    
+        return new Blob([out], { type: 'application/pdf' });
+      };
+      
+      
+      PreviewPDF(){
+        if(this.state.data){
+          if(this.state.type === 'application/pdf'){
+            const blob = this.base64toBlob(this.state.data);
+            const url = URL.createObjectURL(blob);
+
+            
+              return (
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.min.js">
+                <div
+                  style={{
+                    border: '1px solid rgba(0, 0, 0, 0.3)',
+                    height: '750px',
+                  }}>
+                  <Viewer fileUrl={url} />
+                </div>
+                </Worker>
+              );
+        }else{
+          return(
+            <div
+                style={{
+                  border: '1px solid rgba(0, 0, 0, 0.3)',
+                  height: '750px',
+                }}>
+                Please select PDF file
+              </div>
+          );
+        }
+      }
+      }
+
 
     // File content to be displayed after
     // file upload is complete
@@ -94,9 +150,12 @@ class App extends Component {
               <div>
                 <div>
                   <input type="file" onChange={this.onFileChange} />
-                  <button onClick={this.onFileUpload}>שלח קבצים</button>
+                  <button className='btn-btn-submit' onClick={this.onFileUpload}>שלח קבצים</button>
                 </div>
-              {this.fileData()}
+                {this.fileData()}
+              </div>
+              <div>
+                {this.PreviewPDF()}
               </div>
             </form>
           </div>
