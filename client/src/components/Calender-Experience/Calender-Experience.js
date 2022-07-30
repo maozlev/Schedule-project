a28f484caaa9a451681189c9c0ca9fd1cadd9b4b
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2022-07-30 22:56:43 +0300

    Add hebrow on my experience

--- client/src/components/Calender-Experience/Calender-Experience.js
+++ client/src/components/Calender-Experience/Calender-Experience.js
@@ -2,26 +2,35 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer  } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calender-Experience.css"
+import moment from "moment"
+import 'moment/locale/he'; 
+const localizer = momentLocalizer(moment) 
+
 
 const locales = {
     "he": require("date-fns/locale/he"),
 };
-const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-});
 
 export default function CustomizedTables(props) {
     return (
         <Calendar 
-        localizer={localizer} events={props.events} startAccessor="start" endAccessor="end" view='month' views={['month']} style={{ height: 500 }} />
+        localizer={localizer} 
+        events={props.events} 
+        startAccessor="start" 
+        endAccessor="end" 
+        view='month' 
+        views={['month']} 
+        style={{ height: 500 }}
+        messages={{
+            next: "חודש הבא",
+            previous: "חודש קודם",
+            today: "היום",
+          }}
+         />
     );
   }
 
