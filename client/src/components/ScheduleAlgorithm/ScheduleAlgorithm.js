f9f28b6e8e952749f7e006364110589a29280255
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2022-08-15 18:38:32 +0300

    Get the output string in utf8

--- client/src/components/ScheduleAlgorithm/ScheduleAlgorithm.js
+++ client/src/components/ScheduleAlgorithm/ScheduleAlgorithm.js
@@ -4,32 +4,6 @@ import "./ScheduleAlgorithm.css";
 import Button from '@mui/material/Button';
 
 export default function ScheduleAlgorithm(props) {
-    const [file, setFile] = useState(null)
-
-    // On file select (from the pop up)
-    async function onFileChange({target}){
-        if(target.files < 1 || !target.validity.valid){
-          return
-        }
-        this.setState({ selectedFile: target.files[0], type: target.files[0].type });
-        fileToBase64(target.files[0],(err,result) => {
-          if (result){
-            this.setState({ data: result });
-          }
-        })
-        console.log("file chosen")
-      };
-
-    async function fileToBase64(file,cb){
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(file)
-        fileReader.onload = function() {
-          cb(null,fileReader.result)
-        }
-        fileReader.onerror = function (error) {
-          cb(error,null)
-        }
-      };
       const activateAlgo =  async(val) => {
         console.log(val);
         await axios.post("http://localhost:3001/api/activateAlgo/", {
--- server/controllers/activateAlgo.js
+++ server/controllers/activateAlgo.js
@@ -1,14 +1,58 @@
-
+const spawn = require("child_process").spawn;
+const UserTemplate = require("../model/updateDetails")
+const {Data} = require("../../client/src/static/RequestFormData")
 const activateAlgo = async (req, res) => {
   console.log("algorithm start", req.body);
-//   .then((ans, err) => {
-//     if (err) {
-//       console.log("Problem occured!\n" + err.toString());
-//     } else {
-//       console.log("One exp added to ", req.body.newExp.UserName);
-//     }
-//     res.status(200);
-//   });
+  let url_script = "C:/Users/Yosef/Schedule-project/client/src/ScheduleAlgorithm/main.py"
+  let url_cred = "C:/Users/Yosef/Schedule-project/client/src/ScheduleAlgorithm/scheduler-359321-2d5d4c4018a4.json"
+  
+  var all_res;
+
+  function perpareExperiences(){
+    all_exper = []
+    all_res.forEach(element => {
+      let username = await UserTemplate.findOne({ id: element['תעודת זהות']}).exec()
+      if(username){
+        exper = {
+          'Group':,
+          'Area':,
+          'Department':,
+          'Hospital':,
+          'Address':{
+            'City':,
+            'Street':,
+            'Number':
+          },
+          "Contact" : ,
+          "PhoneNumber" : ,
+          "Email" : ,
+          "StartDate" : {
+              "Year": ,
+              "Month" : ,
+              "Day" : 
+          },
+          "EndDate" : {
+              "Year": ,
+              "Month" : ,
+              "Day" : 
+          }
+        }
+        all_exper.push(exper)
+      }
+    });
+  }
+  const pythonProcess = spawn('python',[url_script,url_cred, "2>&1"]);
+  pythonProcess.stdout.on('data', (data) => {
+    // console.log("Data content - " + data.toString())
+    all_res += JSON.parse(data);
+   });
+
+   pythonProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    perpareExperiences();
+  });
+   
+
 };
 
 module.exports = {
